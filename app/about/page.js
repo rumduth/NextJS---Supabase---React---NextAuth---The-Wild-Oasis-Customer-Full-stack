7e3ddde85952799056a56a41c38d0d7cb358df8d
Duth Ng <rumduth1@gmail.com>
2025-05-12 08:57:31 -0500

    fix revalidate

--- app/about/page.js
+++ app/about/page.js
@@ -3,8 +3,7 @@ import About1 from "@/public/about-1.jpg";
 import Link from "next/link";
 import { getCabins } from "../_lib/data-service";
 
-// export const revalidate = 60 * 60 * 24;
-
+export const revalidate = 86400;
 export const metadata = {
   title: "About",
 };
